0a776e15b03fc06a91aaf832a5f8af0a2f8a1ffd
Charuna Amarasinghe <charuna.amarasinghe@qualitapps.com>
2024-02-20 23:35:25 +0530

    first commit

---
+++ CheckBOX/Scripts/HomeService.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('myApp')
+        .controller('ReportCtrl', ['$scope', function ($scope) {
+
+            $scope.portalIdEnabled = true;
+
+            $scope.reporttypes = [
+                { id: 1, code: 'a', symbol: 'Active', name: 'Active' },
+                { id: 2, code: 'p', symbol: 'Pending', name: 'Pending' },
+                { id: 3, code: 'r', symbol: 'Reject', name: 'Reject' },
+                { id: 4, code: 's', symbol: 'Submit', name: 'Submit' }
+            ];
+            
+            $scope.options = [];
+
+            $scope.selectAll = false;
+
+            $scope.toggleTypes = function (index) {
+                $scope.reporttypes[index].checked = !$scope.reporttypes[index].checked;
+                if (!$scope.reporttypes[index].checked) {
+                    $scope.selectAll = false;
+                }
+            };
+
+            $scope.toggleAll = function () {
+                var checked = $scope.selectAll;
+                for (var i = 0; i < $scope.reporttypes.length; i++) {
+                    $scope.options[i] = checked;
+                    $scope.reporttypes[i].checked = checked;
+                }
+            };
+
+
+            $scope.InitFunction = function () {
+                console.log("Success!")
+            }
+        }]);
+})();
